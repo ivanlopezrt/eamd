400e923ad81d6bc6e58a6a52b35268d3da863bba
tatishi <tanshi85@gmail.com>
2023-05-30 18:14:24 +0100

    json loads

--- CheckBoxes.py
+++ CheckBoxes.py
@@ -1,6 +1,6 @@
-import router_state
 import tkinter as tk
 import json
+import router_state
 
 def save_settings():
     settings = {
@@ -14,10 +14,19 @@ def save_settings():
 
 def block_internet():
     myJSON = json.load(open('mydata.json'))
-    myJSON=myJSON['credentials'][0]
+    myJSON = myJSON['credentials'][0]
+
+
 def stop_block_internet():
     myJSON = json.load(open('mydata.json'))
     myJSON = myJSON['credentials'][0]
+
+def router_status():
+    if router_state.isBlocked(what='chatgpt3'):
+        return "ChatGPT is blocked"
+    else:
+        router_state.block(what='chatgpt3')
+        return "ChatGPT is now blocked"
 
 # Create the main window
 window = tk.Tk()
@@ -26,40 +35,33 @@ window.title("Internet Blocker")
 # Create checkboxes
 var_chatgpt = tk.IntVar()
 chk_chatgpt = tk.Checkbutton(window, text="Block ChatGPT", variable=var_chatgpt)
-chk_chatgpt.pack()
+chk_chatgpt.pack(anchor=tk.W)
 
 var_stackoverflow = tk.IntVar()
 chk_stackoverflow = tk.Checkbutton(window, text="Block stackoverflow", variable=var_stackoverflow)
-chk_stackoverflow.pack()
+chk_stackoverflow.pack(anchor=tk.W)
 
 var_github = tk.IntVar()
 chk_github = tk.Checkbutton(window, text="Block github", variable=var_github)
-chk_github.pack()
+chk_github.pack(anchor=tk.W)
 
 var_streaming = tk.IntVar()
-chk_streaming = tk.Checkbutton(window, text="Block strimming platforms", variable=var_streaming)
-chk_streaming.pack()
+chk_streaming = tk.Checkbutton(window, text="Block streaming platforms", variable=var_streaming)
+chk_streaming.pack(anchor=tk.W)
 
 # Create buttons
-btn_block_internet = tk.Button(window, text="Block Internet", command=block_internet)
-btn_block_internet.pack()
+btn_block_internet = tk.Button(window, text="Block Internet", command=block_internet, height=3, width=20)
+btn_block_internet.pack(pady=10)
 
-btn_stop_block_internet = tk.Button(window, text="Stop Block Internet", command=stop_block_internet)
+btn_stop_block_internet = tk.Button(window, text="Stop Block Internet", command=stop_block_internet, height=3, width=20)
 btn_stop_block_internet.pack()
+
+# Create status label
+lbl_status = tk.Label(window, text=router_status())
+lbl_status.pack()
 
 # Save settings when the window is closed
 window.protocol("WM_DELETE_WINDOW", save_settings)
-
-# Load settings from the JSON file (if it exists)
-try:
-    with open("settings.json", "r") as file:
-        settings = json.load(file)
-        var_chatgpt.set(settings.get("Block ChatGPT", 0))
-        var_stackoverflow.set(settings.get("Block stackoverflow", 0))
-        var_github.set(settings.get("Block github", 0))
-        var_streaming.set(settings.get("Block strimming platforms", 0))
-except FileNotFoundError:
-    pass
 
 # Start the main event loop
 window.mainloop()
