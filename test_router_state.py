ed2379981bbd5c60aa22a78ba0298cb0ea4bd640
ivanlopezrt <ivanlopez@riberadeltajo.es>
2023-05-30 18:56:59 +0200

    example of use of router_state.py

--- test_router_state.py
+++ test_router_state.py
@@ -2,11 +2,16 @@ import router_state
 
 #example for Tania
 
+#now two possible exceptions:
+#1. InexistingRuleException -> did connect to router, but rule does not exist
+#2. Exception -> could not connect to router
+
 try:
     if router_state.isBlocked( what='kjdsfhfsdkj'):
         print("ChatGPT is blocked")
     else:
         router_state.change_state(what='sdmjgfsdkfj',blocked=True)
+
 except router_state.InexistingRuleException as e:
     print("Exception: "+str(e))
 except Exception as e:
