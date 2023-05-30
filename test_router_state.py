d234307fad62a54088e261d0670169dcbb34bbeb
ivanlopezrt <ivanlopez@riberadeltajo.es>
2023-05-30 18:56:04 +0200

    example of use of router_state.py

--- test_router_state.py
+++ test_router_state.py
@@ -3,9 +3,11 @@ import router_state
 #example for Tania
 
 try:
-    if router_state.isBlocked( what='chatgpt3'):
+    if router_state.isBlocked( what='kjdsfhfsdkj'):
         print("ChatGPT is blocked")
     else:
-        router_state.change_state(what='chatgpt3',blocked=True)
-except Exception as e:
+        router_state.change_state(what='sdmjgfsdkfj',blocked=True)
+except router_state.InexistingRuleException as e:
     print("Exception: "+str(e))
+except Exception as e:
+    print("Error connecting to router: Exception: "+str(e))
